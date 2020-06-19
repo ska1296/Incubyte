a214622f2e0a4fa81ab9a2644c2bfb258ef5340c
Shubh Ketan Agarwal <ska1296@gmail.com>
2020-06-19 19:25:11 +0530

    Setup basic structure for first case in assessment

---
+++ src/main/com/assessment/firstcase/StringCalculator.java
@@ -0,0 +1,10 @@
+package com.assessment.firstcase;
+
+public class StringCalculator {
+    
+    public int add(String numbers) {
+        
+        return 0;
+    }
+
+}
---
+++ src/main/com/assessment/firstcase/test/StringCalculatorTest.java
@@ -0,0 +1,15 @@
+package com.assessment.firstcase.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class StringCalculatorTest {
+
+    @Test
+    void testAdd() {
+        fail("Not yet implemented");
+    }
+
+}
